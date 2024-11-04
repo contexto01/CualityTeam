6382130ff1f5a054c4517440754a82d7c5b6624d
Daniel Barrera <daniel.barrera02121@hotmail.com>
2024-11-04 16:45:27 -0500

    optimize img

--- src/data/games.ts
+++ src/data/games.ts
@@ -7,11 +7,11 @@ const games: Games = {
       "Embárcate en una aventura épica con 'FALL OF THE GODS', un juego ambientado en la mitología griega. Descubre los misterios y poderes del protagonista, hijo de un dios, mientras ayuda en la guerra del Olimpo.",
     descriptionPage:
       'Embárcate en una aventura épica con FALL OF THE GODS, un juego ambientado en la mitología griega. Descubre los misterios y poderes del protagonista, hijo de un dios, mientras ayuda en la guerra del Olimpo. Explora un mundo de mitos y leyendas, evoluciona de mortal a dios con habilidades únicas, sumérgete en una narrativa envolvente, influye en el destino del mundo con decisiones impactantes y usa poderes divinos en batallas intensas.',
-    image: '/FallOfTheGods.png',
+    image: '/FallOfTheGods.webp',
     screenshots: [
-      '/FallOfTheGodsIMG1.png',
-      '/FallOfTheGodsIMG2.png',
-      '/FallOfTheGodsIMG3.png'
+      '/FallOfTheGodsIMG1.webp',
+      '/FallOfTheGodsIMG2.webp',
+      '/FallOfTheGodsIMG3.webp'
     ],
     features: [
       'Aventura Épica: Explora un mundo de mitos y leyendas.',
@@ -30,8 +30,8 @@ const games: Games = {
       '"BLASPHEMOUS" te lleva a un mundo oscuro y sombrío donde la culpa y el sacrificio son la esencia de su narrativa. Juegas como el Penitente, un guerrero que debe enfrentarse a los terribles enemigos de la tierra de Cvstodia y desentrañar los secretos de su doloroso pasado mientras luchas contra monstruos y jefes aterradores.',
     descriptionPage:
       '"BLASPHEMOUS" te lleva a un mundo oscuro y sombrío donde la culpa y el sacrificio son la esencia de su narrativa. Juegas como el Penitente, un guerrero que debe enfrentarse a los terribles enemigos de la tierra de Cvstodia y desentrañar los secretos de su doloroso pasado mientras luchas contra monstruos y jefes aterradores. Explora un vasto y hermoso mundo de pixel art lleno de secretos y sorpresas, utiliza una mezcla de habilidades, hechizos y movimientos especiales para derrotar a enemigos desafiantes, descubre la historia detrás de la culpa y la penitencia a través de entornos vívidos y diálogos profundos, mejora tus habilidades y personaliza el estilo de juego según tus preferencias, y enfréntate a jefes únicos y peligrosos que pondrán a prueba tus habilidades y estrategia.',
-    image: '/Blasphemous.jpg',
-    screenshots: ['/blasphemousIMG1.jpg', '/blasphemousIMG2.jpg', '/blasphemousIMG3.jpg'],
+    image: '/Blasphemous.webp',
+    screenshots: ['/blasphemousIMG1.webp', '/blasphemousIMG2.webp', '/blasphemousIMG3.webp'],
     features: [
       'Mundo Interconectado: Explora un vasto y hermoso mundo de pixel art lleno de secretos y sorpresas.',
       'Combate Preciso: Utiliza una mezcla de habilidades, hechizos y movimientos especiales para derrotar a enemigos desafiantes.',
@@ -49,8 +49,8 @@ const games: Games = {
       'DEAD CELLS es un juego de acción y plataformas de tipo roguelike. Enfréntate a enemigos desafiantes mientras exploras un mundo en constante cambio. ¡Supera los niveles y descubre secretos ocultos!',
     descriptionPage:
       'DEAD CELLS es un juego de acción y plataformas de tipo roguelike. Enfréntate a enemigos desafiantes mientras exploras un mundo en constante cambio. ¡Supera los niveles y descubre secretos ocultos! Explora un mundo de mitos y leyendas, evoluciona de mortal a dios con habilidades únicas, sumérgete en una narrativa envolvente, influye en el destino del mundo con decisiones impactantes y usa poderes divinos en batallas intensas.',
-    image: '/DeadCells.png',
-    screenshots: ['/deadCellsIMG1.jpg', '/deadCellsIMG2.jpg', '/deadCellsIMG3.jpg'],
+    image: '/DeadCells.webp',
+    screenshots: ['/deadCellsIMG1.webp', '/deadCellsIMG2.webp', '/deadCellsIMG3.webp'],
     features: [
       'Aventura Épica: Explora un mundo de mitos y leyendas.',
       'Desarrollo de Personaje: Evoluciona de mortal a dios con habilidades únicas.',
