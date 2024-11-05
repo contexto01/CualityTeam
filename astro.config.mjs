f16d3daf5ba8f84251d6468926afe35821f62dca
Daniel Barrera <daniel.barrera02121@hotmail.com>
2024-11-05 11:55:01 -0500

    Add sitemap site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cualityteam.netlify.app/',
   integrations: [tailwind(), sitemap()]
-});
+})
